ba852feb5358acefaed49fb347fd31502aff00f1
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-10 01:44:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,25 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    
-    
+
+    try {
+        await client.connect();
+        // console.log('Database Connected')
+        const serviceCollection = client.db('doctors_portal').collection('services');
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+
+
+    }
+    finally {
+
+    }
 }
 run().catch(console.dir);
 
