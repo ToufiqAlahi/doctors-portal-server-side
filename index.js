3425cda625661c3b323da9a2f90e3b1b0b76ebe9
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-10 01:26:25 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -6,6 +7,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.30afqya.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    
+    
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello Doctor!')
