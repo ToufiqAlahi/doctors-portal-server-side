16d0472735c93c6cfcd8f3fdce41428db568943b
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-27 20:23:59 +0600

    Verify JWT function

--- index.js
+++ index.js
@@ -13,17 +13,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next) {
-    // console.log('abc');
     const authHeader = req.headers.authorization;
     if (!authHeader) {
         return req.status(401).send({ message: 'UnAuthorized Access' });
     }
-
-   
-    
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return req.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
 }
-async function run() {
 
+async function run() {
     try {
         await client.connect();
         // console.log('Database Connected')
