adefb157dc09f020c0a7eb9157b923405f298479
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-27 20:41:49 +0600

    bug fixed

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return req.status(401).send({ message: 'UnAuthorized Access' });
+        return res.status(401).send({ message: 'UnAuthorized Access' });
     }
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
         if (err) {
-            return req.status(403).send({ message: 'Forbidden Access' });
+            return res.status(403).send({ message: 'Forbidden Access' });
         }
         req.decoded = decoded;
         next();
@@ -85,7 +85,7 @@ async function run() {
                 res.send(bookings);
             }
             else {
-                return req.status(403).send({ message: 'Forbidden Access' });
+                return res.status(403).send({ message: 'Forbidden access' })
             }
 
         })
